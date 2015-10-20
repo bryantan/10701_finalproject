bff7bd520d8ac71a2ce7a750666b64b379684c2d
bryantan <bryan.es.tan@gmail.com>
2015-10-20 17:01:18 -0400

    simple script to remove unneeded tags

---
+++ remove_text_tags.py
@@ -0,0 +1,13 @@
+import re
+import sys
+
+subs = ['<text.*?>', '</text>', '<fontspec.*?/>', '<page.*?>', '</page>', '<image.*?/>', '<\?.*?\?>', '<!DOCTYPE.*?>', '<pdf2xml.*?>', '</pdf2xml>']
+
+text = ""
+with open(sys.argv[1], "r") as xmlfile:
+    text = xmlfile.read().replace('\n', '')
+
+for sub in subs:
+    text = re.sub(sub, '', text)
+
+print text
